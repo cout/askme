2920d36e97b5b9610c22cc87b1c4de021f37663e
Paul Brannan <curlypaul924@gmail.com>
2011-03-29 04:04:42 -0400

    New project.

---
+++ Askme/src/org/nonstdout/askme/Askme.java
@@ -0,0 +1,15 @@
+package org.nonstdout.askme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Askme extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
