dbc10af292620bcb88017d983a5bebf5d11d4d7f
Paul Brannan <curlypaul924@gmail.com>
2011-03-29 05:37:55 -0400

    Added tts.

--- Askme/src/org/nonstdout/askme/Askme.java
+++ Askme/src/org/nonstdout/askme/Askme.java
@@ -4,14 +4,29 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
-public class Askme extends ListActivity
+public class Askme
+  extends ListActivity
+  implements TextToSpeech.OnInitListener
 {
+  public static final String TAG = "Askme";
+
   // private ArrayList<String> strings_ = new ArrayList<String>();
+
   private ArrayAdapter<String> list_adapter_;
   private static final String[] strings_ = new String[] { "foo", "bar", "baz" };
+
+  private TextToSpeech tts_;
+
+  private Button start_button_;
+  private Button stop_button_;
 
   /** Called when the activity is first created. */
   @Override
@@ -27,5 +42,47 @@ public class Askme extends ListActivity
     final ListView list_view = getListView();
     list_view.setItemsCanFocus(false);
     list_view.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+    tts_ = new TextToSpeech(this, this);
+
+    start_button_ = (Button) findViewById(R.id.button_start);
+    start_button_.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v) {
+        start();
+      }
+    });
+
+    stop_button_ = (Button) findViewById(R.id.button_stop);
+    stop_button_.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v) {
+        stop();
+      }
+    });
+  }
+
+  public void onInit(int status) {
+    if (status == TextToSpeech.SUCCESS) {
+      int result = tts_.setLanguage(Locale.US);
+      if (result == TextToSpeech.LANG_MISSING_DATA ||
+          result == TextToSpeech.LANG_NOT_SUPPORTED)
+      {
+        Log.e(TAG, "Language is not available.");
+      }
+      else
+      {
+        // start_button_.setEnabled(true);
+      }
+    }
+    else
+    {
+      Log.e(TAG, "Could not initialize TextToSpeech.");
+    }
+  }
+
+  private void start() {
+    tts_.speak("Hello", TextToSpeech.QUEUE_FLUSH, null);
+  }
+
+  private void stop() {
   }
 }
